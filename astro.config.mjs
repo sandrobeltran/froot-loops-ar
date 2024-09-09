20b25577f6f17ae7b833319198d27b7431641185
Sandro Beltrán <devsandrobeltran@gmail.com>
2024-09-08 20:15:21 -0500

    adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,22 @@ import { defineConfig } from "astro/config";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sandrobeltran.github.io",
   base: "/froot-loops-ar",
   integrations: [tailwind()],
   server: {
-    host: "0.0.0.0",
+    host: "0.0.0.0"
   },
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
